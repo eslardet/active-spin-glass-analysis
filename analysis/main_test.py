3dfe67efb23d8eb53ec65f1e9253c9626b2f4433
Eloise Lardet <eloise.lardet@gmail.com>
2023-02-01 09:44:29 +0000

    testing

--- analysis/main_test.py
+++ analysis/main_test.py
@@ -1,14 +1,15 @@
 import numpy as np
-import analysis_functions as fun
+import analysis_functions_lattice as fun
 import os
 import matplotlib.pyplot as plt
 import sys
+import time
 
-mode = 'C'
-nPart = 6000
-phi = 0.1
-Pe = 50.0
-K = '10.0'
+mode = "C"
+nPart = 1024
+Rp = 2.0
+rotD = 0.0
+K = -10.0
 seed = 1
 
 # fun.dist_coupling(mode=mode, nPart=nPart, phi=phi, Pe=Pe, K=K, seed=seed, avg_over=avg_over)
@@ -24,9 +25,20 @@ seed = 1
 
 # fun.plot_dist_coupling(mode, nPart, phi, Pe, KAVG, KSTD, seed, avg_over)
 
-fun.snapshot_pos_ex(mode=mode,nPart=nPart,phi=phi,Pe=Pe,K=K,seed=seed)
+# fun.snapshot_pos_ex(mode=mode,nPart=nPart,phi=phi,Pe=Pe,K=K,seed=seed)
 # inparFile, posFile = fun.get_files(mode=mode,nPart=nPart,phi=phi,Pe=Pe,K=K,seed=seed)
 # fun.get_pos_arr(inparFile, posFile, min_T=None, max_T=None)
 
 # fun.snapshot(mode,nPart,phi,Pe,K,seed,view_time=6)
 # fun.animate(mode,nPart,phi,Pe,K,seed,max_T=100)
+
+t0 = time.time()
+p1 = fun.plot_porder_time(mode, nPart, K, Rp, rotD, seed, min_T=0, max_T=10)
+print(time.time() - t0)
+
+
+t1 = time.time()
+p2 = fun.plot_porder_time_large(mode, nPart, K, Rp, rotD, seed, min_T=0, max_T=10)
+print(time.time() - t1)
+
+print(p1 == p2)
