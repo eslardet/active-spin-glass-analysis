78414c3046ff0fdbfe7c451751fc091c7f68cb4f
Eloise Lardet <eloise.lardet@gmail.com>
2023-05-03 11:07:07 +0100

    src vicsek mean field

--- src_vicsek_mf/runV.sh
+++ src_vicsek_mf/runV.sh
@@ -10,30 +10,30 @@
 # Directories #
 ###############
 
-bin_dir=$HOME/Code/2D_ActiveSpinGlass_EL/bin
+bin_dir=$HOME/bin
 # matlab_dir=$bd_dir/codes/matlab
 
 ##############
 # Parameters #
 ##############
 
-nPart=1000
-phi=1.0
-noise=0.20
+nPart=$1
+phi=$2
+noise=$3
 
-seed=1
+seed=$4
 
-vp=1.0
+vp=$5
 
-Rp="MF"
-xTy=1.0
+Rp=$6
+xTy=$7
 
-initMode='R'
+initMode=$8
 # can be:
 #    'R' random, 
 #    'S' restart from previous simulation
 
-couplingMode='G'
+couplingMode=$9
 # can be:
 #    'C' constant, 
 #    'T' for two populations, 
@@ -41,53 +41,39 @@ couplingMode='G'
 #    'F' for normally distributed ferromagnetic, 
 #    'A' for normally distributed antiferromagnetic
 
-K0=0.0
+K0=${10}
 
 # KAA=10.0
 # KAB=0.0
 # KBB=10.0
 
-KAVG=1.0
-STDK=0.0
+KAVG=${10}
+STDK=${11}
 
-dT=0.005
-DT=1.0
-DTex=1.0
-eqT=0.0
-simulT=100.0
+dT=${12}
+DT=${13}
+DTex=${14}
+eqT=${15}
+simulT=${16}
 
-savePos=1
-saveInitPos=0
+savePos=${17}
+saveInitPos=${18}
 saveForce=0
-saveCoupling=0
+saveCoupling=${19} # Need to save couplings to be able to restart sim later for e.g. mode 'G'
 
-intMethod='E'
+intMethod=${20}
 
-# Local
 if [ "${couplingMode}" == "C" ]; then
-    run_dir=$HOME/Code/2D_ActiveSpinGlass_EL/simulation_data/Constant/N${nPart}/phi${phi}_n${noise}/K${K0}/Rp${Rp}/xTy${xTy}/s${seed}
+    run_dir=$HOME/Vicsek_2D/simulation_data/Constant/N${nPart}/phi${phi}_n${noise}/K${K0}/Rp${Rp}/xTy${xTy}/s${seed}
 elif [ "${couplingMode}" == "T" ]; then
-    run_dir=$HOME/Code/2D_ActiveSpinGlass_EL/simulation_data/TwoPopulations/N${nPart}/phi${phi}_n${noise}/K${KAA}/Rp${Rp}/xTy${xTy}/s${seed}
+    run_dir=$HOME/Vicsek_2D/simulation_data/TwoPopulations/N${nPart}/phi${phi}_n${noise}/K${KAA}/Rp${Rp}/xTy${xTy}/s${seed}
 elif [ "${couplingMode}" == "G" ]; then
-    run_dir=$HOME/Code/2D_ActiveSpinGlass_EL/simulation_data/Gaussian/N${nPart}/phi${phi}_n${noise}/K${KAVG}_${STDK}/Rp${Rp}/xTy${xTy}/s${seed}
+    run_dir=$HOME/Vicsek_2D/simulation_data/Gaussian/N${nPart}/phi${phi}_n${noise}/K${KAVG}_${STDK}/Rp${Rp}/xTy${xTy}/s${seed}
 elif [ "${couplingMode}" == "F" ]; then
-    run_dir=$HOME/Code/2D_ActiveSpinGlass_EL/simulation_data/Ferromagnetic/N${nPart}/phi${phi}_n${noise}/K${KAVG}_${STDK}/Rp${Rp}/xTy${xTy}/s${seed}
+    run_dir=$HOME/Vicsek_2D/simulation_data/Ferromagnetic/N${nPart}/phi${phi}_n${noise}/K${KAVG}_${STDK}/Rp${Rp}/xTy${xTy}/s${seed}
 elif [ "${couplingMode}" == "A" ]; then
-    run_dir=$HOME/Code/2D_ActiveSpinGlass_EL/simulation_data/Antiferromagnetic/N${nPart}/phi${phi}_n${noise}/K${KAVG}_${STDK}/Rp${Rp}/xTy${xTy}/s${seed}
+    run_dir=$HOME/Vicsek_2D/simulation_data/Antiferromagnetic/N${nPart}/phi${phi}_n${noise}/K${KAVG}_${STDK}/Rp${Rp}/xTy${xTy}/s${seed}
 fi
-
-# # Cluster
-# if [ "${couplingMode}" == "C" ]; then
-#     run_dir=$HOME/Vicsek_2D/simulation_data/Constant/N${nPart}/phi${phi}_n${noise}/K${K0}/xTy${xTy}/s${seed}
-# elif [ "${couplingMode}" == "T" ]; then
-#     run_dir=$HOME/Vicsek_2D/simulation_data/TwoPopulations/N${nPart}/phi${phi}_n${noise}/K${K0}/xTy${xTy}/s${seed}
-# elif [ "${couplingMode}" == "G" ]; then
-#     run_dir=$HOME/Vicsek_2D/simulation_data/Gaussian/N${nPart}/N${nPart}/phi${phi}_n${noise}/K${K0}/xTy${xTy}/s${seed}
-# elif [ "${couplingMode}" == "F" ]; then
-#     run_dir=$HOME/Vicsek_2D/simulation_data/Ferromagnetic/N${nPart}/N${nPart}/phi${phi}_n${noise}/K${K0}/xTy${xTy}/s${seed}
-# elif [ "${couplingMode}" == "A" ]; then
-#     run_dir=$HOME/Vicsek_2D/simulation_data/Antiferromagnetic/N${nPart}/N${nPart}/phi${phi}_n${noise}/K${K0}/xTy${xTy}/s${seed}
-# fi
 
 
 ###################################
@@ -106,26 +92,33 @@ echo "Starting Vicsek 2D run..."
 
 cd $run_dir
 
-# if [ -e "stats" ]; then
-#     rm 'stats'
-
 if [ ${initMode} == "S" ]; then # Only overwrite initMode and simulT in inpar if restarting from previous simulation
-    sed -i '' "8s/.*/${initMode}/" 'inpar' # extra '' required on MacOS for sed (remove on Linux)
+    sed -i "8s/.*/${initMode}/" 'inpar' # extra '' required on MacOS for sed (remove on Linux)
     if [ "${couplingMode}" == "C" ]; then
-        sed -i '' "14s/.*/${eqT}/" 'inpar'
-        sed -i '' "15s/.*/${simulT}/" 'inpar'
+        sed -i "12s/.*/${DT}/" 'inpar'
+        sed -i "13s/.*/${DTex}/" 'inpar'
+        sed -i "14s/.*/${eqT}/" 'inpar'
+        sed -i "15s/.*/${simulT}/" 'inpar'
     elif [ "${couplingMode}" == "T" ]; then
-        sed -i '' "16s/.*/${eqT}/" 'inpar'
-        sed -i '' "17s/.*/${simulT}/" 'inpar'
+        sed -i "14s/.*/${DT}/" 'inpar'
+        sed -i "15s/.*/${DTex}/" 'inpar'
+        sed -i "16s/.*/${eqT}/" 'inpar'
+        sed -i "17s/.*/${simulT}/" 'inpar'
     elif [ "${couplingMode}" == "G" ]; then
-        sed -i '' "15s/.*/${eqT}/" 'inpar'
-        sed -i '' "16s/.*/${simulT}/" 'inpar'
+        sed -i "13s/.*/${DT}/" 'inpar'
+        sed -i "14s/.*/${DTex}/" 'inpar'
+        sed -i "15s/.*/${eqT}/" 'inpar'
+        sed -i "16s/.*/${simulT}/" 'inpar'
     elif [ "${couplingMode}" == "F" ]; then
-        sed -i '' "15s/.*/${eqT}/" 'inpar'
-        sed -i '' "16s/.*/${simulT}/" 'inpar'
+        sed -i "13s/.*/${DT}/" 'inpar'
+        sed -i "14s/.*/${DTex}/" 'inpar'
+        sed -i "15s/.*/${eqT}/" 'inpar'
+        sed -i "16s/.*/${simulT}/" 'inpar'
     elif [ "${couplingMode}" == "A" ]; then
-        sed -i '' "15s/.*/${eqT}/" 'inpar'
-        sed -i '' "16s/.*/${simulT}/" 'inpar'
+        sed -i "13s/.*/${DT}/" 'inpar'
+        sed -i "14s/.*/${DTex}/" 'inpar'
+        sed -i "15s/.*/${eqT}/" 'inpar'
+        sed -i "16s/.*/${simulT}/" 'inpar'
     fi
 
 else
@@ -177,6 +170,6 @@ else
     echo ${intMethod} >> 'inpar'
 fi
 
-time ${bin_dir}/vicsek_2D_MF inpar
+time ${bin_dir}/vicsek_2D inpar
 
 echo "2D Vicsek run done."
